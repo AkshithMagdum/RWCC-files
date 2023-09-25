96ed604606a0f39deabd8feea243dd1ce318c493
Student <akshith.magdhum@globalschoolsalumni.org>
2023-09-25 11:07:46 +0530

    Initial commit

---
+++ aarogya setu/nooood/android/app/src/main/kotlin/com/example/nooood/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nooood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ rwcc project/rwcc_project/lib/pages/home.dart
@@ -0,0 +1,11 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
